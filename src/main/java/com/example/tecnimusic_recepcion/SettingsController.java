856ae1dd834f7adc8cc658033527eb0c9220cdce
tobistarkv <hugovictorvargasmeza@gmail.com>
2025-10-31 15:33:46 -0600

    Estable después de ajustar el area de ajuste de introducción de texto del pie de página

--- src/main/java/com/example/tecnimusic_recepcion/SettingsController.java
+++ src/main/java/com/example/tecnimusic_recepcion/SettingsController.java
@@ -3,6 +3,7 @@ package com.example.tecnimusic_recepcion;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,7 +20,7 @@ public class SettingsController {
     @FXML
     private PasswordField passwordField;
     @FXML
-    private TextField pdfFooterField;
+    private TextArea pdfFooterField;
 
     private DatabaseConfig dbConfig;
 
