1d0e66a73a3689081116dd813f85863b5761ffad
tobistarkv <hugovictorvargasmeza@gmail.com>
2025-11-09 21:24:03 -0600

    Estable después de ajustar el idioma de los calendarios

--- src/main/java/com/example/tecnimusic_recepcion/Launcher.java
+++ src/main/java/com/example/tecnimusic_recepcion/Launcher.java
@@ -1,7 +1,12 @@
 package com.example.tecnimusic_recepcion;
 
+import java.util.Locale;
+
 public class Launcher {
     public static void main(String[] args) {
+        // Establecer el idioma por defecto a Español
+        Locale.setDefault(new Locale("es", "ES"));
+
         System.setProperty("jdk.xml.totalEntitySizeLimit", "200000");
         tecniMusic.main(args);
     }
